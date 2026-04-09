83290cf6b05e0d7b4c9b90fbb69442757b75dec3
basant <basantkumar72580@gmail.com>
2026-04-09 17:26:04 +0530

    1.0

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL || "http://localhost:3000",
+    import.meta.env.VITE_API_URL || "https://agriculture-management-system-ten.vercel.app/",
 });
 
 api.interceptors.request.use((config) => {
